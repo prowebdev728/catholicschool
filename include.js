353f6929bfdab2d971818802c1fb4e62990970fe
prowebdev728 <proweb.developer2016@yandex.com>
2017-05-25 10:19:33 +0800

    init

--- include.js
+++ include.js
@@ -1,5 +1,3 @@
-<!-- Jquery CDN -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-<!-- Bootstrap CDN -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-
+$(document).ready(function() {
+  
+});
