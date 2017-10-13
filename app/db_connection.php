72a6f50bec4e636e0d753f967458b2b1157c7631
prowebdev728 <proweb.developer2016@yandex.com>
2017-10-13 10:08:14 +0800

    update using PDO

--- app/db_connection.php
+++ app/db_connection.php
@@ -7,12 +7,20 @@ $db_username = "root";
 $db_password = "";
 $db_name = "ada";
 
-$conn = new mysqli($db_servername, $db_username, $db_password, $db_name);
+// $conn = new mysqli($db_servername, $db_username, $db_password, $db_name);
 
-// Check connection
+// // Check connection
 
-if ($conn->connect_error) {
-	die("Connection failed: " . $conn->connect_error);
-} 
+// if ($conn->connect_error) {
+// 	die("Connection failed: " . $conn->connect_error);
+// }
+
+try {
+	$db = new PDO("mysql:host=$db_servername;dbname=$db_name;charset=utf8mb4", $db_username, $db_password);
+	// set the PDO error mode to exception
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+	echo "Error: " . $e->getMessage();
+}
 
 ?>
